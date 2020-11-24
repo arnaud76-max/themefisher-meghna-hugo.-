bf57167b0af3e19ee4b0594121202080bd5392ed
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2020-11-24 08:52:17 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,6 +64,23 @@ jQuery(function ($) {
 			}
 		});
 	}
+	/* ========================================================================= */
+/*	Partenaire Filtering Hook
+/* ============================================================================= */  
+var containerEl = document.querySelector('.shuffle-wrapper');
+if (containerEl) {
+	var Shuffle = window.Shuffle;
+	var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+		itemSelector: '.shuffle-item',
+		buffer: 1
+	});
+	jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+		var input = evt.currentTarget;
+		if (input.checked) {
+			myShuffle.filter(input.value);
+		}
+	});
+}
 
 	/* ========================================================================= */
 	/*	Testimonial Carousel
